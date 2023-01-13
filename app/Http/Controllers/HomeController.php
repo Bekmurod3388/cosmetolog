f991ac867243a38aa0193db2f9a83611ff762218
bekmurod3388 <bekmurod3388@gmail.com>
2023-01-13 08:27:47 +0500

    add customer template

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,9 @@ use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
+    public function index(){
+        return view('home.userpage');
+    }
     public function redirect(Request $request){
         $usertype = Auth::user()->usertype;
         if($usertype=='1'){
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[HomeController::class,'index']);
 
 Route::middleware([
     'auth:sanctum',
