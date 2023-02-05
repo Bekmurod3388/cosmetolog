6a7c3e69b30e7e5d89abd4a68f27e6ffa30c0fc0
Lenovo <asadbek.polatov.02@mail.ru>
2023-02-05 18:57:30 +0500

    bug fixed

--- resources/views/home/app.blade.php
+++ resources/views/home/app.blade.php
@@ -109,11 +109,7 @@
 
 <!-- footer end -->
 <div class="cpy_">
-    <p class="mx-auto">© 2021 All Rights Reserved By <a href="https://html.design/">Free Html Templates</a><br>
-
-        Distributed By <a href="https://themewagon.com/" target="_blank">ThemeWagon</a>
-
-    </p>
+    <p class="mx-auto">© 2023 Все права защищены</p>
 </div>
 
 
